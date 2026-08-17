ffb6bed991fdedd7ebdeea1f4d0fb08362bcafde
Mohamed Rishan Mohamed Ruzan <mohamedruzan.pro@gmail.com>
2026-08-17 23:37:02 +0530

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+//Test Route 
+app.get('/', (req, res) => {
+    res.send("Server is running...");
+});
+
+// DB Connction + Server Start
+const PORT = process.env.PORT || 5000;
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then (() => {
+        console .log("Connected to MongoDB");
+        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    })
+    .catch((error) => console.error(error));
