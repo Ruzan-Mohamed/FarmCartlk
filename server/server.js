bff7722ed1cd4b2e03a19ebbb91cec05027c7ee9
Mohamed Rishan Mohamed Ruzan <mohamedruzan.pro@gmail.com>
2026-08-26 19:12:06 +0530

    Updating server

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true}));
 
 //Test Route 
 app.get('/', (req, res) => {
-    res.send("Server is running...");
+    res.send("FarmCartlk API is running...");
 });
 
 // DB Connction + Server Start
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 5000;
 mongoose
     .connect(process.env.MONGO_URI)
     .then (() => {
-        console .log("Connected to MongoDB");
+        console .log("Connected to MongoDB Successfully");
         app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
     })
     .catch((error) => console.error(error));
