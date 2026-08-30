cba3559af8a3e3aeff4964f341efb0ce3e04ec81
Mohamed Rishan Mohamed Ruzan <mohamedruzan.pro@gmail.com>
2026-08-30 13:16:39 +0530

    Created authContoller and Route

---
+++ server/controllers/authController.js
@@ -0,0 +1,41 @@
+import User from "../models/users.js";
+import bcrypt from "bcryptjs";
+
+const register = async (req, res) => {
+    const {name, email, password, profileImage, address } = req.body;
+
+    if (!name || !email || !password) {
+        return res.status(400).json({ message: "Name, email and password are required" });
+    }
+
+    if (password.length < 8) {
+        return res.status(400).json({ message: "Password must be at least 8 characters"});
+    }
+
+    try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = new User({
+            name, 
+            email,
+            password: hashedPassword,
+            role: "buyer",
+            profileImage,
+            address
+        });
+
+        await newUser.save();
+        return res.status(201).json({ message: "User registered successfully" });
+
+    }
+    catch (error) {
+        return res.status(500).json({ message: "Server error" });
+    }
+};
+
+export { register };
