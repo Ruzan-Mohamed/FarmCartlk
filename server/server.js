272fac20f2a5fb26c040f868b738e2f68f3224f2
Mohamed Rishan Mohamed Ruzan <mohamedruzan.pro@gmail.com>
2026-09-04 00:29:51 +0530

    feat: integrate Supabase storage image upload

--- server/server.js
+++ server/server.js
@@ -1,35 +1,40 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
+require("dotenv").config();
 
-const authRoute = require("./routes/authRoute.js")
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const authRoute = require("./routes/authRoute.js");
 const uploadRoutes = require("./routes/uploadRoutes");
-
-dotenv.config();
 
 const app = express();
 
-//Middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-//Test Route 
-app.get('/', (req, res) => {
-    res.send("FarmCartlk API is running...");
+// Test Route
+app.get("/", (req, res) => {
+  res.send("FarmCartlk API is running...");
 });
 
-app.use('/api/auth', authRoute);
+// Routes
+app.use("/api/auth", authRoute);
 app.use("/api/upload", uploadRoutes);
 
-// DB Connction + Server Start
+// DB Connection + Server Start
 const PORT = process.env.PORT || 5000;
 
 mongoose
-    .connect(process.env.MONGO_URI)
-    .then (() => {
-        console .log("Connected to MongoDB Successfully");
-        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-    })
-    .catch((error) => console.error(error));
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB Successfully");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
